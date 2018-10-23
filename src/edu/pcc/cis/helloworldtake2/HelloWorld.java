9af9d9cac8b9d3682a4a39f4c9f8ad4b3d7e0629
Elijah Probst <elijah.probst@pcc.edu>
2018-10-23 14:39:57 -0700

    print date

--- src/edu/pcc/cis/helloworldtake2/HelloWorld.java
+++ src/edu/pcc/cis/helloworldtake2/HelloWorld.java
@@ -1,10 +1,13 @@
 package edu.pcc.cis.helloworldtake2;
 
+import java.util.Date;
+
 /**
  * Created by Elijah Probst and Cara Tang on 10/23/2018
  */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
