074af8fdd9bd2540baf1c762e0f9da993e614fd1
Elijah Probst <elijah.probst@pcc.edu>
2018-10-23 14:34:24 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworldtake2/HelloWorld.java
+++ src/edu/pcc/cis/helloworldtake2/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworldtake2;
 
+/**
+ * Created by Elijah Probst and Cara Tang on 10/23/2018
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
